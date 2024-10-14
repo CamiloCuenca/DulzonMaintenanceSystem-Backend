0cb695515b9a4d19c3c297e2d61492d3acaf6049
Juan Camilo Cuenca Sepulveda <camilocuenca1810@gmail.com>
2024-10-13 21:44:30 -0500

    commit inicial

---
+++ src/main/java/co/edu/uniquindio/dulzonmaintenancesystem/DulzonMaintenanceSystemApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.dulzonmaintenancesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DulzonMaintenanceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DulzonMaintenanceSystemApplication.class, args);
+    }
+
+}
